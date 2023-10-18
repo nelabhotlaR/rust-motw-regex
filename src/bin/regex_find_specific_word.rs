b30fd143c96dd0772695a455726b44b25122b3c9
Raghava Nelabhotla <raghava.nelabhotla@gmail.com>
2023-10-18 13:09:37 +0530

    rust examples for the regex

---
+++ src/bin/regex_find_specific_word.rs
@@ -0,0 +1,28 @@
+use regex::Regex;
+
+fn main() {
+    // Define the word you want to find
+    let word_to_find = "specific";
+
+    // Create a Regex object for the word
+    let re = Regex::new(&format!(r"\b{}\b", regex::escape(word_to_find)));
+
+    // Check if the regex compilation was successful
+    let re = match re {
+        Ok(re) => re,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
+        }
+    };
+
+    // The input sentence
+    let sentence = "This is a sentence with a specific word.";
+
+    // Check if the word exists in the sentence
+    if re.is_match(sentence) {
+        println!("The word '{}' exists in the sentence.", word_to_find);
+    } else {
+        println!("The word '{}' does not exist in the sentence.", word_to_find);
+    }
+}
